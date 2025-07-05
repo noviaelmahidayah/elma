d28892b46aae0fa723c251baa69f8dba1b496342
Nunu <Nunu@gmail.com>
2025-07-05 20:08:50 +0700

    Update 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,23 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $super_admin = User::where('email', 'superadmin@local.com')->first();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        $super_admin = User::whare('email', 'superadmin@local.com')->first();
-
-        if(empty($super_admin)){
+        if (empty($super_admin)) {
             User::create([
                 'name' => 'Super Admin',
                 'email' => 'superadmin@local.com',
-                'password' => Hash::make('1212'),
-            ]); 
+                'password' => Hash::make('12345678'),
+            ]);
         }
-            
-        
     }
 }
